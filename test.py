a47fb5c10c1b8bd758cd24843e25638d065d31ed
marcusyh <marcusyh@gmail.com>
2019-04-24 18:39:15 +0900

    tmp: save work process

--- test.py
+++ test.py
@@ -10,3 +10,31 @@ if __name__ == '__main__':
     kj_dict, kj_list = get_source()
     wiki_text = get_youmi(kj_list, update_flag)
 
+
+    
+    import re
+    h1 = open('/tmp/youmi.txt', 'w')
+    h2 = open('/tmp/youmi2.txt', 'w')
+    for k, v in wiki_text.items():
+        s = v.string.replace('\n', '\\n')
+                #'(?P<name2>[^=><!]*(?:<!--[^<!>]*-->)?[^=><!]*(?:<!--[^<!>]*-->)?)' \
+        x = re.match(\
+                '(?P<name0> *(={2,5}[^=]*={2,5})* *(\\\\n)* *(={2,5}[^=]*={2,5})* *(\\\\n)*)? *' \
+                '(?P<name1>([^=><!]*(<[^<!>]*>)?[^=><!]*)*)' \
+                '(?P<name2><!--[^<!>]*-->)*' \
+                '(?P<name3> *={2,4}\{\{[^=]*\}\}={2,4} *)?' \
+                '(?P<name4>.*)?' \
+                     , s)
+        if not x:
+            print(k, v)
+            continue
+        y = x.groupdict()
+        z = k + '\t' + '\t'.join([str(y[k]) for k in sorted(y)]) + '\n'
+        h1.write('%s\t%s\n' %(k, y['name1']))
+        h2.write(z)
+        #if y['name5'] and y['name5'].find('*') >= 0:
+        #    z = '%s\t%s\t%s\t%s\t%s\t%s\t%s\n' %(k, y['name0'], y['name1'], y['name2'], y['name3'], y['name4'], y['name5'])
+        #    print(z)
+    h1.close()
+    h2.close()
+        #print(z)
